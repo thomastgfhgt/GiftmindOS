4536ad7984cd417d81350f430797e1e6ea973877
thomas thiriot-lesne <thomasthiriot-lesne@device-218.home>
2026-05-20 10:38:09 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import dynamic from "next/dynamic";
+const GiftMindOS = dynamic(() => import("../components/GiftMindOS"), { ssr: false });
+export default function Home() {
+  return <GiftMindOS />;
+}
